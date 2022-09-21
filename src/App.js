f96aa3d349df033461302292244e8efd19d9d6e2
David <davidongaroh@gmail.com>
2022-09-21 22:14:55 +0300

    new div

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import Products from './components/Products';
 function App() {
   return (
     <div className="App">
+      <div className='container'>
+        <img src='' alt=''/>
+        <h2>Exotic Furniture</h2>
+        <p>Choose from a pool of well-crafted & stylish premium sets online</p>
+      </div>
     <Products/>
     </div>
   );
