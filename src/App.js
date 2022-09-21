4a9cd1f21fb28d8edab2f2079c2148b90387e513
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-09-21 20:48:13 +0300

    added router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { Route, Switch, useHistory } from 'react-router-dom';
 import './App.css';
+import ProductPage from './components/ProductPage';
 
 function App() {
+  let history=useHistory()
+
+  function handleNavigate(to){
+    history.push(to)
+  }
   return (
     <div className="App">
-      
+      {/* add a button here with #handleNavigate passing the route */}
+      <button onClick={()=>handleNavigate('/products')}>product</button>
+      <Switch>
+        <Route to="/products">
+          <ProductPage/>
+        </Route>
+        {/* add your Route here */}
+      </Switch>
     </div>
   );
 }
