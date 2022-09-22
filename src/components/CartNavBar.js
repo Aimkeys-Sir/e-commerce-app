0ed846b351e2c7def8a1511776905ade8e67a64b
davidnene <davienesh4@gmail.com>
2022-09-22 23:23:26 +0300

    Import cart-nav-bar.css

--- src/components/CartNavBar.js
+++ src/components/CartNavBar.js
@@ -1,3 +1,18 @@
+import './style/cart-nav-bar.css'
+
 export default function CartNavBar() {
-    return
+    return (
+        <div>
+            <div>
+                
+            </div>
+            <div className="topnav">
+                <a className='shopname' href='#home'>Tarlet</a>
+                <a href="#shop">Shop</a>
+                <a href="#journal">Journal</a>
+                <a href="#about-us">About Us</a>
+                <a href="#contact">Contact</a>
+            </div>
+        </div>
+    )
 }
