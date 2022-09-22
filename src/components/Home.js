cebb9d082773ecf8770d849a169680e9e3cb5299
Precious <Preshomondi@gmail.com>
2022-09-22 14:42:55 +0300

    divide home component into grids

---
+++ src/components/Home.js
@@ -0,0 +1,46 @@
+import React from "react";
+import "./Home.css"
+
+function Home(){
+    return(
+        <div className="home">
+            <div className="grid one">
+                <h1>Clock</h1>
+            </div>
+            <div className="grid two">
+            <h1>Chair</h1>
+            </div>
+            <div className="grid three">
+            <h1>Lamp</h1> 
+            </div>
+            <div className="grid four">
+            <h1>Table</h1> 
+            </div>
+            <div className="grid five">
+            <h1>Couch</h1> 
+            </div>
+            <div className="grid six">
+            <h1>Desk</h1> 
+            </div>
+            <div className="grid seven">
+            <h1>Cupboards</h1> 
+            </div>
+            <div className="grid eight">
+            <h1>Beds</h1> 
+            </div>
+            <div className="grid nine">
+            <h1>Racks</h1> 
+            </div>
+            <div className="grid ten">
+            <h1>Contact</h1> 
+            </div>
+            <div className="grid eleven">
+            <h1>Newsletter</h1> 
+            </div>
+    
+
+        </div>
+    )
+}
+
+export default Home
